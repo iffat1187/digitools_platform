78dfdb4dfbff734afbb364ec757f8014a89a9562
iffatjahan1187 <iffatjahan1470@gmail.com>
2026-05-05 22:43:54 +0600

    all product section slightly complete

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,60 +1,60 @@
 import { Suspense, useState } from "react";
-// import ALLTools from "../components/allTools/AllTools";
+import ALLTools from "../components/allTools/AllTools";
 import Banner from "../components/banner/Banner";
 import Choose from "../components/chooseSection/Choose";
 import CTASection from "../components/ctaSection/CTASection";
 import Footer from "../components/footer/Footer";
 import Navbar from "../components/navbar/Navbar";
 import Pricing from "../components/pricingSection/Pricing";
-// import Rating from "../components/ratingSection/Rating";
+import Rating from "../components/ratingSection/Rating";
 import { ToastContainer } from "react-toastify";
 
 
 
-// const productDataFetch = fetch("/productData.json").then((res) => res.json());
+const productDataFetch = fetch("/productData.json").then((res) => res.json());
 
 const Home = () => {
 
-const [addToCart,setAddToCart]=useState([])
+    const [addToCart, setAddToCart] = useState([])
 
-  return (
-    <div>
-        
-      <Navbar addToCart={addToCart}/>
+    return (
+        <div>
 
-      <Banner />
- 
-      
+            <Navbar addToCart={addToCart} />
 
-      {/* <div className="bg-linear-to-r from-[#4F39F6]  to-[#9B5CF6]">
-        <Rating />
-      </div> */}
+            <Banner />
 
-      {/* <Suspense
-        fallback={
-          <div className="flex justify-center py-20">
-            <span className="loading loading-spinner loading-lg text-primary"></span>
-          </div>
-        }
-      >
-        <ALLTools productDataFetch={productDataFetch} addToCart={addToCart} setAddToCart={setAddToCart}/>
-      </Suspense> */}
 
-      <Choose />
 
-      <Pricing />
+            <div className="bg-linear-to-r from-[#4F39F6]  to-[#9B5CF6]">
+                <Rating />
+            </div>
 
-      <div className="bg-linear-to-r from-[#4F39F6]  to-[#9B5CF6]">
-        <CTASection />
-      </div>
+            <Suspense
+                fallback={
+                    <div className="flex justify-center py-20">
+                        <span className="loading loading-spinner loading-lg text-primary"></span>
+                    </div>
+                }
+            >
+                <ALLTools productDataFetch={productDataFetch} addToCart={addToCart} setAddToCart={setAddToCart} />
+            </Suspense>
 
-      <div className="bg-[#101727]">
-        <Footer />
-      </div>
+            <Choose />
 
-      <ToastContainer/> 
-    </div>
-  );
+            <Pricing />
+
+            <div className="bg-linear-to-r from-[#4F39F6]  to-[#9B5CF6]">
+                <CTASection />
+            </div>
+
+            <div className="bg-[#101727]">
+                <Footer />
+            </div>
+
+            <ToastContainer />
+        </div>
+    );
 };
 
 export default Home;
